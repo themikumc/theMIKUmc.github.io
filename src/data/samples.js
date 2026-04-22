e8f055566615a1496ccc466f7834929b4221180e
--global <themikumc@gmail.com>
2026-04-22 11:17:37 -0700

    edit

--- src/data/samples.js
+++ src/data/samples.js
@@ -12,8 +12,20 @@ export const samples = [
       { id: 'tiktok', label: 'tiktok', href: 'https://www.tiktok.com/@themikumc' }
     ]
   },
-  {
+   {
     id: 2,
+    category: 'editor',
+    type: 'external',
+    src: 'https://img.youtube.com/vi/S1XqeZD8dUQ/maxresdefault.jpg',
+    name: 'minecraft pvp 💙',
+    caption: 'my first edit',
+    description: 'minecraft pvp edit',
+    links: [
+      { id: 'youtube', label: 'youtube', href: 'https://youtu.be/S1XqeZD8dUQ' }
+    ]
+  },
+  {
+    id: 3,
     category: 'dev',
     type: 'external',
     src: 'https://img.youtube.com/vi/3wanIjfMSzc/maxresdefault.jpg',
@@ -23,16 +35,6 @@ export const samples = [
     links: [
       { id: 'youtube', label: 'youtube', href: 'https://youtu.be/3wanIjfMSzc' }
     ]
-  },
-  {
-    id: 3,
-    category: 'dev',
-    type: 'video',
-    src: '/samples/mod-loader-tools.mp4',
-    poster: '/samples/mod-loader-thumb.jpg',
-    name: 'sample02',
-    caption: 'lorem ipsum',
-    description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.'
   },
   {
     id: 4,
