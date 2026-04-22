1bd69b4a3e6e45e2d862b9199f2424e83e5c7869
--global <themikumc@gmail.com>
2026-04-22 12:03:49 -0700

    edit

--- src/data/samples.js
+++ src/data/samples.js
@@ -35,34 +35,5 @@ export const samples = [
     links: [
       { id: 'youtube', label: 'youtube', href: 'https://youtu.be/3wanIjfMSzc' }
     ]
-  },
-  {
-    id: 4,
-    category: 'editor',
-    type: 'video',
-    src: '/samples/pvp-core-showcase.mp4',
-    poster: '/samples/pvp-core-thumb.jpg',
-    name: 'sample0',
-    caption: 'lorem ipsum',
-    description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.'
-  },
-  {
-    id: 5,
-    category: 'editor',
-    type: 'image',
-    src: '/samples/skyblock-ui.jpg',
-    name: 'sample01',
-    caption: 'lorem ipsum',
-    description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.'
-  },
-  {
-    id: 6,
-    category: 'editor',
-    type: 'video',
-    src: '/samples/mod-loader-tools.mp4',
-    poster: '/samples/mod-loader-thumb.jpg',
-    name: 'sample02',
-    caption: 'lorem ipsum',
-    description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.'
   }
 ]
