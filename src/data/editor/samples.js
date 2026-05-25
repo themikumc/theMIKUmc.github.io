4099c6502a5175c5b1ef48e31024879b46d9c5e7
--global <themikumc@gmail.com>
2026-05-25 00:26:44 -0700

    yes

---
+++ src/data/editor/samples.js
@@ -0,0 +1,32 @@
+export const editorSamples = [
+  {
+    id: 1,
+    type: 'external',
+    src: '/pfpbig.jpg',
+    name: 'theMIKUmc',
+    caption: 'youtube & tiktok channels',
+    description: 'my channel, containing many well-edited shorts!',
+    links: [
+      { id: 'youtube', label: 'youtube', href: 'https://www.youtube.com/@themikumc' },
+      { id: 'tiktok', label: 'tiktok', href: 'https://www.tiktok.com/@themikumc' }
+    ]
+  },
+  {
+    id: 2,
+    type: 'external',
+    src: 'https://img.youtube.com/vi/S1XqeZD8dUQ/maxresdefault.jpg',
+    name: 'minecraft pvp 💙',
+    caption: 'my first edit',
+    description: 'minecraft pvp edit',
+    links: [{ id: 'youtube', label: 'youtube', href: 'https://youtu.be/S1XqeZD8dUQ' }]
+  },
+  {
+    id: 3,
+    type: 'external',
+    src: 'https://img.youtube.com/vi/RP_0qZgvf0Y/maxresdefault.jpg',
+    name: 'skied dating show ❤️ (ItzRealMe)',
+    caption: 'mc longform editing',
+    description: 'minecraft dating show w/ itzrealme',
+    links: [{ id: 'youtube', label: 'youtube', href: 'https://youtu.be/RP_0qZgvf0Y' }]
+  }
+]
