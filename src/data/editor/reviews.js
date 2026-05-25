8da4569e4f4d26c1126c4d92ed7201ca4bae5e6f
--global <themikumc@gmail.com>
2026-05-25 08:28:27 -0700

    yes

--- src/data/editor/reviews.js
+++ src/data/editor/reviews.js
@@ -1,10 +1,10 @@
 export const editorReviews = [
-  {
-    id: 1,
-    name: 'skied',
-    href: 'https://www.youtube.com/@Skiedlmao',
-    role: 'upcoming mc youtuber',
-    rating: 5,
-    quote: 'goated editor'
-  }
+  // {
+  //   id: 1,
+  //   name: 'skied',
+  //   href: 'https://www.youtube.com/@Skiedlmao',
+  //   role: 'upcoming mc youtuber',
+  //   rating: 5,
+  //   quote: 'goated editor'
+  // }
 ]
