0592dc096bd917fac9834f765bfa8e4f5d302163
--global <themikumc@gmail.com>
2026-05-28 22:43:08 -0700

    yes

--- src/data/editor/reviews.js
+++ src/data/editor/reviews.js
@@ -1,10 +1,10 @@
 export const editorReviews = [
-  // {
-  //   id: 1,
-  //   name: 'skied',
-  //   href: 'https://www.youtube.com/@Skiedlmao',
-  //   role: 'upcoming mc youtuber',
-  //   rating: 5,
-  //   quote: 'goated editor'
-  // }
+  {
+    id: 1,
+    name: 'skied',
+    href: 'https://www.youtube.com/@Skiedlmao',
+    role: 'upcoming mc youtuber',
+    rating: 5,
+    quote: 'The editing was good, this beat my last videos max view count of around 270k in 2 days, and it was good I really appreciate Miku for all the work they\'ve done for me personally. :D'
+  }
 ]
